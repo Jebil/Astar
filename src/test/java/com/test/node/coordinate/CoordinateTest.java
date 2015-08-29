2f06f24a3fc613200e75591136d1dd07ea050349
unknown <jebil4u@gmail.com>
2015-08-29 12:08:44 +0530

    TestCase change for XYCoordinate.

--- src/test/java/com/test/node/coordinate/CoordinateTest.java
+++ src/test/java/com/test/node/coordinate/CoordinateTest.java
@@ -11,10 +11,8 @@ import com.multichoice.node.coordinate.impl.XYCoordinate;
 public class CoordinateTest {
 	@Test
 	public void checkXYCoordinates() {
-		XYCoordinate coordinate = new XYCoordinate();
-		XYCoordinate coordinate1 = new XYCoordinate();
-		coordinate.setCoordinates(10, 5);
-		coordinate1.setCoordinates(10, 5);
+		XYCoordinate coordinate = new XYCoordinate(10,5);
+		XYCoordinate coordinate1 = new XYCoordinate(10,5);
 		assertEquals(coordinate.getX(), 10);
 		assertEquals(coordinate.getY(), 5);
 		assertEquals(coordinate, coordinate1);
